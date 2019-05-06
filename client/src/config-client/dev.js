c505636da109ee26cf49e28d6363598da0b8f658
Sean Flores <codewilling@gmail.com>
2019-05-06 11:06:14 -0700

    remove dev.js from gitignore

---
+++ client/src/config-client/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientID: process.env.GOOGLECLIENTID,
+    googleClientSecret: process.env.GOOGLESECRET,
+    googleAPIKey: process.env.GOOGLEAPIKEY,
+    mongodbURI: process.env.MONGODB_URI,
+    cookieKey: process.env.COOKIEKEY,
+    linkedInClientID: process.env.LINKED_IN_CLIENT_ID,
+    linkedInClientSecret: process.env.LINKED_IN_CLIENT_SECRET,
+    facebookAppID: process.env.FACEBOOK_APP_ID,
+    facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
+  }
